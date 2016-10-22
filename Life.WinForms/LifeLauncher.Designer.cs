f401c7fbe197e8c4328c9d38f6eb1ac4f1ff47a0
Dmitry Nelyubin <dmitry.nelyubin@sidenis.com>
2016-10-22 20:59:21 +0300

    Added WinForms Demo shell

---
+++ Life.WinForms/LifeLauncher.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace DD.Life.WinForms
+{
+    partial class LifeLauncher
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtBody = new System.Windows.Forms.TextBox();
+            this.cmbPatterns = new System.Windows.Forms.ComboBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.tmrReevaluation = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // txtBody
+            // 
+            this.txtBody.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBody.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBody.Location = new System.Drawing.Point(12, 47);
+            this.txtBody.Multiline = true;
+            this.txtBody.Name = "txtBody";
+            this.txtBody.Size = new System.Drawing.Size(640, 373);
+            this.txtBody.TabIndex = 0;
+            this.txtBody.Text = "Example for design time\r\n*   *\r\n * *\r\n  *";
+            // 
+            // cmbPatterns
+            // 
+            this.cmbPatterns.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmbPatterns.FormattingEnabled = true;
+            this.cmbPatterns.Location = new System.Drawing.Point(12, 12);
+            this.cmbPatterns.Name = "cmbPatterns";
+            this.cmbPatterns.Size = new System.Drawing.Size(582, 21);
+            this.cmbPatterns.TabIndex = 1;
+            this.cmbPatterns.SelectedIndexChanged += new System.EventHandler(this.cmbPatterns_SelectedIndexChanged);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStart.Location = new System.Drawing.Point(600, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(23, 21);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "*";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.EhlStart);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStop.Location = new System.Drawing.Point(629, 12);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(23, 21);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "X";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.EhlStop);
+            // 
+            // tmrReevaluation
+            // 
+            this.tmrReevaluation.Interval = 250;
+            this.tmrReevaluation.Tick += new System.EventHandler(this.tmrReevaluation_Tick);
+            // 
+            // LifeLauncher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 432);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.cmbPatterns);
+            this.Controls.Add(this.txtBody);
+            this.Name = "LifeLauncher";
+            this.Text = "The Life";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBody;
+        private System.Windows.Forms.ComboBox cmbPatterns;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Timer tmrReevaluation;
+    }
+}
+
---
+++ Life.WinForms/LifeLauncher.cs
@@ -0,0 +1,90 @@
+﻿using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
+using DD.CommonPatterns;
+
+namespace DD.Life.WinForms
+{
+    public sealed partial class LifeLauncher : Form
+    {
+        private Grid _grid;
+        private readonly int _xRange = 16;
+        private readonly int _yRange = 16;
+
+        private readonly string _appName;
+
+        public LifeLauncher()
+        {
+            InitializeComponent();
+
+            txtBody.Text = null;
+
+            _appName = Text;
+
+            InitializePatterns();
+        }
+
+        private void InitializePatterns()
+        {
+            var commonPatternsAssembly = typeof(Spaceships).Assembly;
+            var q = from t in commonPatternsAssembly.GetTypes()
+                    where t.IsAbstract && t.IsSealed
+                    from f in t.GetFields(BindingFlags.Public | BindingFlags.Static)
+                    where f.FieldType == typeof(string)
+                    select new PatternInfo(t.Name + "." + f.Name, (string)f.GetValue(null));
+
+            var availablePatterns = q.ToArray();
+            cmbPatterns.DataSource = availablePatterns;
+        }
+
+        public class PatternInfo
+        {
+            public string FullName { get; }
+            public string Value { get; }
+
+            public PatternInfo(string fullName, string value)
+            {
+                FullName = fullName;
+                Value = value;
+            }
+
+            public override string ToString()
+            {
+                return FullName;
+            }
+        }
+
+        private void cmbPatterns_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            EhlStop();
+
+            var selectedPattern = (PatternInfo)cmbPatterns.SelectedItem;
+            txtBody.Text = selectedPattern.Value;
+
+            // ReSharper disable once LocalizableElement
+            Text = $"{_appName} ({selectedPattern.FullName})";
+
+            var pattern = Pattern.Parse(selectedPattern.Value, _xRange, _yRange);
+            _grid = new Grid(_xRange, _yRange, pattern);
+        }
+
+        private void EhlStart(object sender, System.EventArgs e)
+        {
+            txtBody.Text = _grid.ToString();
+
+            tmrReevaluation.Start();
+        }
+
+        private void EhlStop(object sender = null, System.EventArgs e = null)
+        {
+            tmrReevaluation.Stop();
+        }
+
+        private void tmrReevaluation_Tick(object sender, System.EventArgs e)
+        {
+            _grid.Reevaluate();
+
+            txtBody.Text = _grid.ToString();
+        }
+    }
+}
