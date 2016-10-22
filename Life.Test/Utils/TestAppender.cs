003ac37c70b0ffb47704cc89ac39ec1660025314
Dmitry Nelyubin <dmitry.nelyubin@sidenis.com>
2016-10-22 13:32:57 +0300

    Added project files with first necessities.

---
+++ Life.Test/Utils/TestAppender.cs
@@ -0,0 +1,29 @@
+﻿using log4net.Core;
+using log4net.Filter;
+using log4net.Layout;
+
+namespace DD.Life.Test.Utils
+{
+    /// <remarks>
+    /// Since NUnit 3, output of TraceAppender doesn't end up in ReSharper test runner output.
+    /// </remarks>
+    public class TestAppender : log4net.Appender.ConsoleAppender
+    {
+        public TestAppender()
+        {
+            Threshold = Level.Debug;
+
+            SetLayout();
+            AddFilters();
+        }
+
+        private void SetLayout()
+        {
+            Layout = new PatternLayout("%message%newline");
+        }
+
+        private void AddFilters()
+        {
+        }
+    }
+}
